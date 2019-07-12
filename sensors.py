daa52d87fb560590d2ee0ef3c5319a6164be3915
bequite <valentin8dev@gmail.com>
2019-07-12 10:42:07 +0300

    fix sensors gui

--- sensors.py
+++ sensors.py
@@ -5,30 +5,26 @@ from flask import (
 from .db import get_db
 
 bp = Blueprint('sensors', __name__)
+db_data = []
 
 
 @bp.route('/sensors')
 def index():
-    return render_template('sensors/sensors.html')
-
-
-@bp.route('/sensors/temp_bme280')
-def show_tem_bme280():
+    global db_data
     db = get_db()
     db_data = db.execute(
         'SELECT *'
         ' FROM bme280'
-        ' ORDER BY created DESC'
+        ' ORDER BY created DESC LIMIT 20'
     ).fetchall()
+    return render_template('sensors/sensors.html', db_data=db_data)
+
+
+@bp.route('/sensors/temp_bme280')
+def show_tem_bme280():
     return render_template('sensors/temp_bme280.html', db_data=db_data)
 
 
 @bp.route('/sensors/hum_bme280')
 def show_hum_bme280():
-    db = get_db()
-    db_data = db.execute(
-        'SELECT *'
-        ' FROM bme280'
-        ' ORDER BY created DESC'
-    ).fetchall()
     return render_template('sensors/hum_bme280.html', db_data=reversed(db_data))
