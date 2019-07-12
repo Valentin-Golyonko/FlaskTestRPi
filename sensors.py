e422b7b08767320b1c1997d83594701bedbd822a
bequite <valentin8dev@gmail.com>
2019-07-13 01:44:35 +0300

    gui graphic

--- sensors.py
+++ sensors.py
@@ -1,47 +1,45 @@
+import json
+
 from flask import (
-    Blueprint, render_template
-)
+    Blueprint, render_template,
+    redirect, url_for)
 
 from db import get_db, close_db
 from color_log.Log_Color import *
 
 bp = Blueprint('sensors', __name__)
-db_data = []
 
 
 @bp.route('/sensors')
 def index():
     log_verbose("sensors: index()")
-    global db_data
+    global db_data, _id, t, h, p, c
     cur = get_db().cursor()
     db_data = cur.execute(
         'SELECT *'
         ' FROM bme280'
-        ' ORDER BY created DESC LIMIT 20'
+        ' ORDER BY created DESC LIMIT 50'
     ).fetchall()
     close_db()
-    return render_template('sensors/sensors.html', db_data=[db_data])
+
+    _id = [i['id'] for i in db_data]
+    t = [i['temperature'] for i in db_data]
+    h = [i['humidity'] for i in db_data]
+    p = [i['pressure'] for i in db_data]
+    c = [i['created'].strftime('%x %X') for i in db_data]
+    # log_info("\tcreated: %s %s" % (c, type(c[0])))
+
+    return render_template('sensors/sensors.html', db_data=[db_data, _id, t, h, p, c])
+    # return redirect(url_for('sensors.index'))
 
 
 @bp.route('/sensors/temp_bme280')
 def show_tem_bme280():
     log_verbose("sensors: show_tem_bme280()")
-    _id = [i['id'] for i in db_data]
-    t = [i['temperature'] for i in db_data]
-    h = [i['humidity'] for i in db_data]
-    p = [i['pressure'] for i in db_data]
-    c = [i['created'].strftime('%Y-%m-%d %H:%M:%S') for i in db_data]
-    log_info("\tcreated: %s %s" % (c, type(c[0])))
     return render_template('sensors/temp_bme280.html', db_data=[db_data, _id, t, h, p, c])
 
 
 @bp.route('/sensors/hum_bme280')
 def show_hum_bme280():
     log_verbose("sensors: show_hum_bme280()")
-    _id = [i['id'] for i in db_data]
-    t = [i['temperature'] for i in db_data]
-    h = [i['humidity'] for i in db_data]
-    p = [i['pressure'] for i in db_data]
-    c = [i['created'].strftime('%Y-%m-%d %H:%M:%S') for i in db_data]
-    log_info("\tcreated: %s %s" % (c, type(c[0])))
-    return render_template('sensors/hum_bme280.html', db_data=reversed([db_data, _id, t, h, p, c]))
+    return render_template('sensors/hum_bme280.html', db_data=[reversed(db_data), _id, t, h, p, c])
