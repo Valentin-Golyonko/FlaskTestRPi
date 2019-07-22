fbcfda5ebe5cd31bb2c282c7a17b3b82278fc340
Valentin <valentin8dev@gmail.com>
2019-07-22 08:31:13 +0300

    Update keys.py

--- api_keys/keys.py
+++ api_keys/keys.py
@@ -1,10 +1,10 @@
 # OWM
-owm_api_key = "08d9c655de0deb39070de66272941267"
+owm_api_key = ""
 
 # twitter:
-consumer_key = "Gv9O40FwHcnfZSNtcE2edNcKs"
-consumer_secret = "VpNMZRm2hZ7xtQDxcMZaCClG673JLsGFIsyz4zTZ21hTo1MsZE"
-access_token = "339642245-FWK0OI3FzvdJr56dnnyfrgmKl0rYqrjFx1KX6lH6"
-access_token_secret = "bSdPQjl2oIGvCUF5e8wncM6gbP49NMyMc8HyXuPe9y4AS"
+consumer_key = ""
+consumer_secret = ""
+access_token = ""
+access_token_secret = ""
 
 # https://www.instagram.com/instagram/
