7f4a896d5f4c4d66254f59bc7da3e468a69abc35
Valentin <valentin8dev@gmail.com>
2020-03-25 11:22:12 +0300

    added: celery periodic task

--- WebApp/HomeBoxApp/celery.py
+++ WebApp/HomeBoxApp/celery.py
@@ -1,8 +1,9 @@
 import os
 
 from celery import Celery
-
 # set the default Django settings module for the 'celery' program.
+from celery.schedules import crontab
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'HomeBoxApp.settings')
 
 app = Celery('HomeBoxApp')
@@ -20,3 +21,23 @@ app.autodiscover_tasks()
 @app.task(bind=True)
 def debug_task(self):
     print('Request: {0!r}'.format(self.request))
+
+
+@app.on_after_configure.connect
+def setup_periodic_tasks(sender, **kwargs):
+    # Calls test('hello') every 10 seconds.
+    sender.add_periodic_task(10.0, test.s('hello'), name='add every 10')
+
+    # Calls test('world') every 30 seconds
+    sender.add_periodic_task(30.0, test.s('world'), expires=10)
+
+    # Executes every Monday morning at 7:30 a.m.
+    sender.add_periodic_task(
+        crontab(hour=11, minute=00, day_of_week=3),
+        test.s('Happy Mondays!'),
+    )
+
+
+@app.task
+def test(arg):
+    print(arg)
