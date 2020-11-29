db86b42d11a5f372a2fd1209c01077291c772ed2
Valentin <v.halionka@nord-soft.com>
2020-11-30 00:55:10 +0300

    added: docker configs

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mkdir logs
 python manage.py migrate
-python manage.py collectstatic --no-input
+#python manage.py collectstatic --no-input
 rm logs/*.pid
 sleep 10
 celery multi start worker -A config -c4 -B -l info --logfile=./logs/%n.log --pidfile=./logs/%n.pid
