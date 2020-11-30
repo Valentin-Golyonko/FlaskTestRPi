877046869e154170070943f4cb84cc9dc2ff3ec5
Valentin <valentin8dev@gmail.com>
2020-12-01 02:11:27 +0300

    minor updates

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,8 @@
 mkdir logs
 rm logs/*.pid
 python manage.py migrate
-#python manage.py collectstatic --no-input
-#python manage.py runserver 192.168.0.102:8000
+python manage.py collectstatic --no-input
 sleep 5
 celery multi start worker -A config -c4 -B -l info --logfile=./logs/%n.log --pidfile=./logs/%n.pid
+sleep 5
+python manage.py runserver 192.168.0.102:8000
