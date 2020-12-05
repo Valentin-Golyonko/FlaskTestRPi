9a87734415d464f153c4780e41ffbdebc1bdecbd
Valentin <v.halionka@nord-soft.com>
2020-12-06 00:50:10 +0300

    upd: celery config

--- config/celery_sh/celery.sh
+++ config/celery_sh/celery.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 source /home/pi/HomeBox/venv/bin/activate
-exec "celery worker -A config -c4 -B -l info --logfile=./logs/%n.log --pidfile=./logs/%n.pid"
+exec "celery -A config.celery:app worker -c4 -B -l info --logfile=./logs/%n.log --pidfile=./logs/%n.pid"
