b14a9ba1bf756d9a2beff47befdfbbd75ef85abd
Valentin <v.halionka@nord-soft.com>
2020-12-05 19:35:24 +0300

    upd: docker

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,4 @@ python manage.py collectstatic --no-input
 sleep 5
 celery multi start worker -A config -c4 -B -l info --logfile=./logs/%n.log --pidfile=./logs/%n.pid
 sleep 5
-python manage.py runserver 192.168.0.102:8000
+python manage.py runserver 0.0.0.0:8000
