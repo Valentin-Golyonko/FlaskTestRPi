f75a5fdecbd6e3914291f5816987d217d37b2380
Valentin <v.halionka@nord-soft.com>
2020-12-06 19:16:26 +0300

    upd: gunicorn.sh

--- config/gunicorn/start_gunicorn.sh
+++ config/gunicorn/start_gunicorn.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 source /home/pi/HomeBox/venv/bin/activate
+python manage.py migrate
+python manage.py collectstatic --no-input
+rm logs/*.pid
+sleep 5
 exec gunicorn -c "/home/pi/HomeBox/config/gunicorn/gunicorn_config.py" config.wsgi
