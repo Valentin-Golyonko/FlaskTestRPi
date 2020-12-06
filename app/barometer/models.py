8af96c3643690c08285996e1e8410435789e952e
Valentin <v.halionka@nord-soft.com>
2020-12-06 19:04:50 +0300

    upd: Barometer model

--- app/barometer/models.py
+++ app/barometer/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 
 class Barometer(models.Model):
-    temperature = models.DecimalField(max_digits=5, decimal_places=2)
+    temperature_c = models.DecimalField(max_digits=5, decimal_places=2)
     humidity = models.DecimalField(max_digits=5, decimal_places=2)
-    pressure_pha = models.DecimalField(max_digits=6, decimal_places=2)
+    pressure_hpa = models.DecimalField(max_digits=6, decimal_places=2)
 
     class Meta:
         verbose_name = 'Barometer'
