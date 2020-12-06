be2457e6466da0113eb633b95179a3879b64557d
Valentin <v.halionka@nord-soft.com>
2020-12-06 20:45:45 +0300

    upd: Device model

--- app/core/models.py
+++ app/core/models.py
@@ -8,8 +8,8 @@ class Device(models.Model):
     device_type = models.PositiveSmallIntegerField(choices=Choices.DEVICE_TYPE_CHOICES)
     sub_type = models.PositiveSmallIntegerField(choices=Choices.DEVICE_SUB_TYPE_CHOICES)
     address_type = models.PositiveSmallIntegerField(choices=Choices.DEVICE_ADDRESS_TYPE_CHOICES)
-    i2c_address = models.CharField(max_length=5, default='', verbose_name='I2C address', null=True)
-    ip_address = models.GenericIPAddressField(default='', null=True)
+    i2c_address = models.CharField(max_length=5, default='', verbose_name='I2C address', blank=True, null=True)
+    ip_address = models.GenericIPAddressField(default='', blank=True, null=True)
 
     class Meta:
         verbose_name = 'Device'
