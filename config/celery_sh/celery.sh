ba10f7487e7e1dcc9cabc3c2f35690b7a0e55339
Valentin <v.halionka@nord-soft.com>
2020-12-24 07:54:12 +0300

    upd: celery run

--- config/celery_sh/celery.sh
+++ config/celery_sh/celery.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 source /home/pi/HomeBox/venv/bin/activate
-celery -A config.celery:app worker -c4 -B -l info --logfile=./logs/%n.log --pidfile=./logs/%n.pid
+celery -A config.celery:app worker -c4 -B -l warning --logfile=./logs/%n.log --pidfile=./logs/%n.pid
