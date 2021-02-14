e27d1a0090d6b0f4e6ac43339c81e081b2bc7eeb
Valentin <v.halionka@nord-soft.com>
2021-02-14 17:50:45 +0300

    added: utility functions

---
+++ app/core/scripts/utils.py
@@ -0,0 +1,24 @@
+import logging
+from datetime import date
+from time import perf_counter
+
+from django.utils.timezone import localtime
+
+logger = logging.getLogger(__name__)
+
+
+def get_local_datetime_as_str(date_obj: date) -> str:
+    return localtime(date_obj).strftime('%Y-%m-%d %H:%M')
+
+
+def time_it(foo_name: str):
+    def decor_0(foo):
+        def decor_1(*args, **kwargs):
+            time_0 = perf_counter()
+            result = foo(*args, **kwargs)
+            logger.debug('time_it(): %s = %.4f' % (foo_name, perf_counter() - time_0))
+            return result
+
+        return decor_1
+
+    return decor_0
