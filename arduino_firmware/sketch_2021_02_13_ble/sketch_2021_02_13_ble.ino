13ebb2727743d3b4530772e1624d595feb49c215
Valentin <v.halionka@nord-soft.com>
2021-03-16 22:04:08 +0300

    firmware upd: added Ping - Pong between server and client

--- arduino_firmware/sketch_2021_02_13_ble/sketch_2021_02_13_ble.ino
+++ arduino_firmware/sketch_2021_02_13_ble/sketch_2021_02_13_ble.ino
@@ -6,6 +6,7 @@ const uint8_t RX = 7;
 const uint8_t TX = 6;
 const uint8_t BLUEPIN = 3;
 bool blue_pin_up = false;
+bool builtin_pin_up = false;
 String some_data = "";
 
 SoftwareSerial SerialBLE(RX, TX); // RX,TX on arduino board
@@ -18,45 +19,48 @@ ReadLoggingStream loggingStream(SerialBLE, Serial);
 void ListenBlt() {
   if (SerialBLE.available() > 0) {
     deserializeJson(doc_in, loggingStream);
-    //    loggingStream.read();
-
-    send_data();
   }
-}
-
-void send_data() {
-  StaticJsonDocument<200> doc_out;
-  long timestamp = millis();
-  int value = digitalRead(BLUEPIN);
-  doc_out["timestamp"] = timestamp;
-  doc_out["value"] = value;
-  serializeJson(doc_out, SerialBLE);
 }
 
 void setup() {
   pinMode(BLUEPIN, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin(9600);         // UART speed
   SerialBLE.begin(9600);
 }
 
 void loop() {
+
   ListenBlt();        // if the data came from SerialBLE
+
   unsigned long currentMillis = millis();
+
   if (currentMillis - previousMillis_1 >= 1000) { // period from android app
     previousMillis_1 = currentMillis;
 
-    String hello = doc_in.getMember("hello");
-    if (!doc_in.isNull()) {  // .isNull() ;   .containsKey("")
-      Serial.println("hello: " + String(hello));
-      doc_in = {};
-    }
+    PingPong();
 
-    if (blue_pin_up) {
-      digitalWrite(BLUEPIN, LOW);
-      blue_pin_up = false;
+    if (builtin_pin_up) {
+      digitalWrite(LED_BUILTIN, LOW);
+      builtin_pin_up = false;
+
     } else {
-      digitalWrite(BLUEPIN, HIGH);
-      blue_pin_up = true;
+      digitalWrite(LED_BUILTIN, HIGH);
+      builtin_pin_up = true;
     }
+  }
+}
+
+void PingPong() {
+  String ping = doc_in.getMember("ping");
+
+  if (!doc_in.isNull() && ping == "ping") {  // .isNull() ;   .containsKey("")
+    Serial.println("ping: " + String(ping));
+
+    StaticJsonDocument<200> doc_out;
+    doc_out["ping"] = "pong";
+    serializeJson(doc_out, SerialBLE);
+
+    doc_in = {};
   }
 }
