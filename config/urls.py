ceb7d1a89b3ddc8cb85541af25f7717bef10c867
Valentin <v.halionka@nord-soft.com>
2021-05-13 18:01:26 +0300

    minor upd

--- config/urls.py
+++ config/urls.py
@@ -5,6 +5,7 @@ from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from app.core.views import MainPageView, LogInView
+from config.settings import DEBUG
 
 urlpatterns = [
     path('', MainPageView.as_view(), name='main_page'),
@@ -22,3 +23,8 @@ urlpatterns = [
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if DEBUG:
+    import debug_toolbar
+
+    urlpatterns.append(path('__debug__/', include(debug_toolbar.urls)))
