c6b78e3c7c566c9821eb4420bb0200fa080c1174
Valentin <v.halionka@nord-soft.com>
2021-06-30 12:06:08 +0300

    added: Barometer Admin

--- app/barometer/admin.py
+++ app/barometer/admin.py
@@ -1,1 +1,22 @@
 from django.contrib import admin
+
+from app.barometer.models import Barometer
+from config.settings import DEBUG
+
+if DEBUG:
+    @admin.register(Barometer)
+    class BarometerAdmin(admin.ModelAdmin):
+        list_display = (
+            'device',
+            'temperature_c',
+            'humidity',
+            'pressure_hpa',
+            'time_created',
+        )
+        readonly_fields = (
+            'temperature_c',
+            'humidity',
+            'pressure_hpa',
+            'device',
+            'time_created',
+        )
