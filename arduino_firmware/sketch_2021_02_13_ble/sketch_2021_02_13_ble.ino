3659ac4b2b56646fc22a8f3aa5ce5abd084222a5
Valentin <v.halionka@nord-soft.com>
2021-06-30 14:54:22 +0300

    minor upd

--- arduino_firmware/sketch_2021_02_13_ble/sketch_2021_02_13_ble.ino
+++ arduino_firmware/sketch_2021_02_13_ble/sketch_2021_02_13_ble.ino
@@ -1,3 +1,7 @@
+/// install ArduinoJson
+/// install StreamUtils
+///
+
 #include <SoftwareSerial.h>
 #include <ArduinoJson.h>
 #include <StreamUtils.h>
@@ -7,19 +11,13 @@ const uint8_t TX = 6;
 const uint8_t BLUEPIN = 3;
 bool blue_pin_up = false;
 bool builtin_pin_up = false;
-
-SoftwareSerial SerialBLE(RX, TX); // RX,TX on arduino board
-
+const uint16_t default_update_time = 1000;
 unsigned long previousMillis_1 = 0;
+
+SoftwareSerial SerialBLE(RX, TX); // RX, TX on arduino board
 
 StaticJsonDocument<200> doc_in;
 ReadLoggingStream loggingStream(SerialBLE, Serial);
-
-void ListenBlt() {
-  if (SerialBLE.available() > 0) {
-    deserializeJson(doc_in, loggingStream);
-  }
-}
 
 void setup() {
   pinMode(BLUEPIN, OUTPUT);
@@ -34,19 +32,18 @@ void loop() {
 
   unsigned long currentMillis = millis();
 
-  if (currentMillis - previousMillis_1 >= 1000) { // period from android app
+  if (currentMillis - previousMillis_1 >= default_update_time) { // period from android app
     previousMillis_1 = currentMillis;
 
     PingPong();
 
-    if (builtin_pin_up) {
-      digitalWrite(LED_BUILTIN, LOW);
-      builtin_pin_up = false;
+    BuiltinPinOnOff();
+  }
+}
 
-    } else {
-      digitalWrite(LED_BUILTIN, HIGH);
-      builtin_pin_up = true;
-    }
+void ListenBlt() {
+  if (SerialBLE.available() > 0) {
+    deserializeJson(doc_in, loggingStream);
   }
 }
 
@@ -61,5 +58,16 @@ void PingPong() {
     serializeJson(doc_out, SerialBLE);
 
     doc_in = {};
+  }
+}
+
+void BuiltinPinOnOff() {
+  if (builtin_pin_up) {
+    digitalWrite(LED_BUILTIN, LOW);
+    builtin_pin_up = false;
+
+  } else {
+    digitalWrite(LED_BUILTIN, HIGH);
+    builtin_pin_up = true;
   }
 }
