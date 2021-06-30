3f1771c5affdc0caecb33bf231c13b7584dc982d
Valentin <v.halionka@nord-soft.com>
2021-06-30 14:54:05 +0300

    added: rgb device data to the main page

--- app/core/main_page_data/main_page.py
+++ app/core/main_page_data/main_page.py
@@ -5,6 +5,7 @@ from django.utils.timezone import localtime
 from app.barometer.models import Barometer
 from app.barometer.serializers import BarometerSerializer
 from app.owm_forecast.models import Forecast
+from app.rgb_control.rgb_control_scripts.get_rgb_divice import GetRGBDevice
 
 logger = logging.getLogger(__name__)
 
@@ -15,13 +16,14 @@ class MainPage:
     def get_main_page_data(cls) -> dict:
         return {**cls.latest_in_home(),
                 **cls.forecast(),
+                "rgb_device": GetRGBDevice.device_serialized_data(),
                 }
 
     @staticmethod
     def latest_in_home() -> dict:
         barometer_obj = Barometer.objects.last()
         barometer_data = BarometerSerializer(barometer_obj).data
-        barometer_data['time_created'] = localtime(value=barometer_obj.time_created).time()
+        barometer_data['time_created'] = localtime(barometer_obj.time_created)
         return {'barometer_data': barometer_data}
 
     @staticmethod
---
+++ app/rgb_control/rgb_control_scripts/get_rgb_divice.py
@@ -0,0 +1,24 @@
+from django.templatetags.tz import localtime
+
+from app.core.choices import Choices
+from app.core.models import Device
+from app.core.serializers import DeviceSerializer
+
+
+class GetRGBDevice:
+
+    @staticmethod
+    def ble_led_strip_alarm() -> Device:
+        return Device.objects.filter(
+            device_type=Choices.DEVICE_TYPE_LED_STRIP,
+            sub_type=Choices.DEVICE_SUB_TYPE_RGB_STRIP_WITH_ALARM,
+            address_type=Choices.DEVICE_ADDRESS_TYPE_BLUETOOTH,
+            mac_address__isnull=False,
+        ).first()
+
+    @staticmethod
+    def device_serialized_data():
+        device_obj = GetRGBDevice.ble_led_strip_alarm()
+        device_data = DeviceSerializer(device_obj).data
+        device_data['last_update'] = localtime(device_obj.last_update)
+        return device_data
