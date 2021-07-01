a3faedb72cd236ce405bc1cd5b6cce4401b93bad
Valentin <quitevi@gmail.com>
2021-07-01 22:22:38 +0300

    upd: RGBControlView with 'rgb_device'

--- app/rgb_control/views.py
+++ app/rgb_control/views.py
@@ -2,20 +2,25 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from rest_framework import status
 from rest_framework.generics import GenericAPIView
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.renderers import TemplateHTMLRenderer
 from rest_framework.response import Response
 
+from app.rgb_control.rgb_control_scripts.get_rgb_divice import GetRGBDevice
 from config.settings import LOGOUT_REDIRECT_URL
 
 
 class RGBControlView(LoginRequiredMixin, GenericAPIView):
     login_url = LOGOUT_REDIRECT_URL
-    renderer_classes = [TemplateHTMLRenderer]
+    # renderer_classes = [TemplateHTMLRenderer]
     template_name = 'rgb_control/rgb_control_main.html'
     permission_classes = (IsAuthenticated,)
     pagination_class = None
 
     @staticmethod
     def get(request, *args, **kwargs):
-
-        return Response(data={'some_data': 'some_data'}, status=status.HTTP_200_OK)
+        return Response(
+            data={
+                'some_data': 'some_data',
+                "rgb_device": GetRGBDevice.device_serialized_data(),
+            },
+            status=status.HTTP_200_OK
+        )
